cab47509e5b16a99dc997e9e8dd4622be2506b2f
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2025-02-22 23:34:49 +0530

    Update Navbar.jsx

--- FrontendTemplate/src/components/Navbar.jsx
+++ FrontendTemplate/src/components/Navbar.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import "./Navbar.css";
-// import { Link } from "react-router-dom";
-// const Navbar = () => {
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+const Navbar = () => {
   
-//   const titleStyle = {
-//     fontSize: "60px",
-//     paddingTop: "50px",
-//     textAlign:"center"
-//   };
+  const titleStyle = {
+    fontSize: "60px",
+    paddingTop: "50px",
+    textAlign:"center"
+  };
   
-//   return (
-//     <nav>
-//       <ul>
-//         <Link to="/home">Home</Link>
-//         <Link to="/about">About</Link>
-//         <Link to="/another">Another</Link>
-//         <Link to="/contact">Contact</Link>
-//         <button type="submit" className="btn btn-secondary">Login</button>
-//         <button type="submit" className="btn btn-primary">SignUp</button>
-//       </ul>
-//       <h1 style={titleStyle}>RoadmapAI</h1>
-//     </nav>
-//   );
-// };
+  return (
+    <nav>
+      <ul>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/another">Another</Link>
+        <Link to="/contact">Contact</Link>
+        <button type="submit" className="btn btn-secondary">Login</button>
+        <button type="submit" className="btn btn-primary">SignUp</button>
+      </ul>
+      <h1 style={titleStyle}>RoadmapAI</h1>
+    </nav>
+  );
+};
 
-// export default Navbar;
+export default Navbar;
 
 // import React, { useState } from "react";
 // import { Link } from "react-router-dom";
