f97c69aebaf4a012bdd2caa5f41607867830588f
manojkumar-13-official <manojkumaratarumugam@gmail.com>
2025-02-22 14:51:11 +0530

    navbar

--- FrontendTemplate/src/App.jsx
+++ FrontendTemplate/src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react"
-import Home from "./components/Home"
-import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Another from "./pages/Another"
+import Contact from "./pages/Contact"
+import Navbar from "./components/Navbar"
 function App() {
   
   return (
-    <>
-      <Home/>
-    </>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/home' element={<Home/>}>Home</Route>
+        <Route path='/about' element={<About/>}>About</Route>
+        <Route path='/another' element={<Another/>}>Another</Route>
+        <Route path='/contact' element={<Contact/>}>Contact</Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ FrontendTemplate/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>RoadmapAI</h1>
+      <ul>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/another">Another</Link>
+        <Link to="/contact">Contact</Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
